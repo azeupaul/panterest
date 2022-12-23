1cb42094a3e241a8ec7a0ea118a2f2446d040a9c
azeupaul <azeupaul@gmail.com>
2022-12-23 17:29:59 +0100

    Add pins entity, controller and display all pins in homepage

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class PinsController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(PinRepository $pinRepository): Response
     {
-        return $this->render('pins/index.html.twig');
+        return $this->render('pins/index.html.twig', [
+            'pins' => $pinRepository->findAll()
+        ]);
     }
 }
