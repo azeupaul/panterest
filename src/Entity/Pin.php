e4a54a915e4ccf2d7f922813e3c6dbc96cf83f42
azeupaul <azeupaul@gmail.com>
2022-12-30 00:03:51 +0100

    Add vich uplaoder bundle and edit config

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -6,11 +6,14 @@ use App\Entity\Traits\Timestampable;
 use App\Repository\PinRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: PinRepository::class)]
 #[ORM\Table(name: 'pins')]
 #[ORM\HasLifecycleCallbacks]
+#[Vich\Uploadable]
 class Pin
 {
     use Timestampable;
@@ -32,6 +35,9 @@ class Pin
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $imageName = null;
+
+    #[Vich\UploadableField(mapping: 'products', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
 
     public function getId(): ?int
     {
@@ -72,5 +78,24 @@ class Pin
         $this->imageName = $imageName;
 
         return $this;
+    }
+
+    /**
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->setUpdatedAt(new \DateTimeImmutable);
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
     }
 }
